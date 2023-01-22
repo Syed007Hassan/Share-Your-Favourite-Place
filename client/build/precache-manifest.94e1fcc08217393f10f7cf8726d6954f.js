f6cc1a098ad0264aed19046d93e206155fe2dba3
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-01-22 20:13:47 +0500

    build added

---
+++ client/build/precache-manifest.94e1fcc08217393f10f7cf8726d6954f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e068ac0dc6bb4cbcaa8b58c62017c955",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df6774dd82281dd8d779",
+    "url": "/static/css/main.1de46aec.chunk.css"
+  },
+  {
+    "revision": "f829e115e4e9d7d42c3b",
+    "url": "/static/js/2.25cfb851.chunk.js"
+  },
+  {
+    "revision": "df6774dd82281dd8d779",
+    "url": "/static/js/main.87e861b0.chunk.js"
+  },
+  {
+    "revision": "c365dd7c495663b5e597",
+    "url": "/static/js/runtime-main.92d67ae5.js"
+  }
+]);
